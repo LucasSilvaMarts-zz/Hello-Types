2dde8617838b9eee67f473be752eb129b3c0a66f
Lucas Silva <martssilvapro@gmail.com>
2021-11-09 10:34:38 -0300

    controller de criacao de mensagem e rota implementados

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import EmailService from "../services/EmailService";
 
 const users = [
   { name: 'Lucas', email: 'lucassilva@vilaapps.com.br' },
@@ -8,5 +9,16 @@ export default {
   async index(req: Request, res: Response) {
 
     return res.json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+
+    emailService.sendEmail(
+      { name: 'Lucas', email: 'lucassilva@vilaapps.com.br' },
+      { subject: 'Bem-vindo ao sistema com types', body: 'ta curtindo?' },
+    );
+
+    res.send();
   }
 };
